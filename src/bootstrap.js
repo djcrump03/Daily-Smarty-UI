21e62f151bddedfe083d4f5350501a74d8bf4515
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-07-02 22:03:57 -0400

    connect recent posts to redux

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
@@ -15,12 +16,10 @@ import './style/main.scss';
 import Home from './components/home';
 import Results from './components/results';
 
-
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        
         <Switch>
             <Route path='/' exact component={Home}/>
             <Route path='/results' component={Results}/>
@@ -29,4 +28,5 @@ function main() {
     </Provider>
     , document.querySelector('.app-wrapper'));
 }
+
 document.addEventListener('DOMContentLoaded', main);
