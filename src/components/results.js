e3ab5caa1678aa5b251ec770639f07e3eed64958
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-07-05 15:53:44 -0400

    modified searchbar component to take onSubmit function as a parm

--- src/components/results.js
+++ src/components/results.js
@@ -3,12 +3,15 @@ import Logo from './logo';
 import SearchBar from './searchBar';
 
 class Results extends Component {
+    handleSearchBarSubmit(query) {
+
+    }
     render() {
         return (
             <div>
                 
                 <Logo size= {55}></Logo>
-                <SearchBar/>
+                <SearchBar onSubmit={(query) =>this.handleSearchBarSubmit(query)}/>
             </div>
         )
     }
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,38 +1,26 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import {withRouter} from 'react-router-dom';
+import { Field, reduxForm} from 'redux-form';
+import { withRouter } from 'react-router-dom';
 class SearchBar extends Component {
 
-    handleFormSubmit = function ({ query }) {
-        console.log('trying to handle submit for query', query);
-   
-        this.props.history.push('/results');
+    handleFormSubmit = function({query}) {
+        this.props.onSubmit(query);
     }
 
     renderInput(field) {
         return <input type="text" placeholder="Search DailySmarty" {...field.input} />
     }
-
     render() {
-
         const { handleSubmit } = this.props;
-
         return (
-
-
             <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-                <Field name="query" component={this.renderInput} />
+                <Field name="query" component={this.renderInput}/>
             </form>
-
-
         )
     }
 }
-
 SearchBar = reduxForm({
     form: 'searchBar'
 })(SearchBar);
-
-SearchBar = withRouter (SearchBar);
-
+SearchBar = withRouter(SearchBar);
 export default SearchBar;
