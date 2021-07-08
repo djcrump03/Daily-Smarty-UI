c0eac4ec0eec8c2c3aca072330eaa6b9f7b40cab
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-07-08 13:11:22 -0400

    fixed some grid styles

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import { Field, reduxForm} from 'redux-form';
 import { withRouter } from 'react-router-dom';
 class SearchBar extends Component {
-
     handleFormSubmit = function({query}) {
         this.props.onSubmit(query);
     }
-
     renderInput(field) {
         return <input type="text" placeholder="Search DailySmarty" {...field.input} />
     }
     render() {
         const { handleSubmit } = this.props;
+
         return (
             <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-                <Field name="query" component={this.renderInput}/>
+                <div className="search-bar__wrapper">
+                    <Field name="query" component={this.renderInput}/>
+                    <p>Press return to search</p>
+                </div>
             </form>
         )
     }
