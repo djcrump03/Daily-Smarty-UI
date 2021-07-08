d9b2f97d9b18ad695af5d58dd69880faf729de7b
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-07-08 13:43:08 -0400

    implemented font awesome into searchbar

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -5,12 +5,13 @@ class SearchBar extends Component {
     handleFormSubmit = function({query}) {
         this.props.onSubmit(query);
     }
+
     renderInput(field) {
-        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+        return <input type="text" placeholder="&#xf002; Search DailySmarty" {...field.input} />
     }
+
     render() {
         const { handleSubmit } = this.props;
-
         return (
             <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <div className="search-bar__wrapper">
@@ -25,4 +26,14 @@ SearchBar = reduxForm({
     form: 'searchBar'
 })(SearchBar);
 SearchBar = withRouter(SearchBar);
+
+    
+          
+            
+    
+
+          
+    
+    
+  
 export default SearchBar;
