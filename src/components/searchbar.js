9612ef90a8b15fcd77b3c6a86c00c1279ac29421
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-07-08 14:56:06 -0400

    styled results component searchbar

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -5,15 +5,14 @@ class SearchBar extends Component {
     handleFormSubmit = function({query}) {
         this.props.onSubmit(query);
     }
-
     renderInput(field) {
         return <input type="text" placeholder="&#xf002; Search DailySmarty" {...field.input} />
     }
-
     render() {
         const { handleSubmit } = this.props;
+
         return (
-            <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+            <form className={`search-bar search-bar__${this.props.page}`} onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <div className="search-bar__wrapper">
                     <Field name="query" component={this.renderInput}/>
                     <p>Press return to search</p>
@@ -26,14 +25,5 @@ SearchBar = reduxForm({
     form: 'searchBar'
 })(SearchBar);
 SearchBar = withRouter(SearchBar);
-
-    
-          
-            
-    
-
-          
-    
-    
   
 export default SearchBar;
